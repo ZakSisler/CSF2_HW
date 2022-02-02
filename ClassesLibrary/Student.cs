50ff68b31da271774c736e91ee258e2951bceca6
Zakary Sisler <zakarysisler@outlook.com>
2022-02-01 20:01:42 -0600

    I made my student object class and then tested it in my tester program cs file.

---
+++ ClassesLibrary/Student.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesLibrary
+{
+    public class Student
+    {
+        private string _firstName;
+        private string _lastName;
+        private string _id;
+        private float _gpa;
+
+        public string FirstName
+        {
+            get { return _firstName; }
+            set { _firstName = value; }
+        }
+        public string LastName
+        {
+            get { return _lastName; }
+            set { _lastName = value; }
+        }
+        public string Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+        public float Gpa
+        {
+            get { return _gpa; }
+            set { _gpa = value; }
+        }
+
+        //FQTOR
+        public Student(string firstName, string lastName, string id, float gpa)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Id = id;
+            Gpa = gpa;
+        }
+
+        //Default un-QTOR
+        public Student() { }
+
+        //ToString()
+        public override string ToString()
+        {
+            //return base.ToString();
+            return string.Format("First Name: {0} " +
+                "\nLast Name: {1}" +
+                "\nID: {2}" +
+                "\nGPA: {3}",
+                FirstName, LastName, Id, Gpa);
+                
+        }
+
+       
+
+    }
+}
---
+++ TesterProgram/CSF2_HWLab.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassesLibrary;
+
+namespace TesterProgram
+{
+    class CSF2_HWLab
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("CSF2 HW LAB!");
+
+            Student s1 = new Student();
+            s1.FirstName = "Frodo";
+            s1.LastName = "Baggins";
+            s1.Id = "h0bb1t";
+            s1.Gpa = 3.2f;
+
+        }
+    }
+}
