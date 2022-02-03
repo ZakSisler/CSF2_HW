84b7b5d7547856cfab58e57697363eefeebd8f6e
Zakary Sisler <zakarysisler@outlook.com>
2022-02-02 23:31:55 -0600

    initial commit

---
+++ ClassesLibrary/CreditCardAccount.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesLibrary
+{
+    public class CreditCardAccount
+    {
+        private int _accountNumber;
+        public Customer _customerInfo;//source control probs?
+        private decimal _balance;
+        private bool _isPastDue;
+        private decimal _annualInterestRate;
+    }
+}
